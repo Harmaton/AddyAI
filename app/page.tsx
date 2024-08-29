388efdd86dfa5cdfbd694044bcc59fd91f23112c
Harmaton <njagiiharmaton@gmail.com>
2024-08-29 11:30:41 +0300

    new test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      ADDY AI
-    Sample env  {url} + NODE ENV {process.env.NODE_ENV}
+    Sample env  {url}, {process.env.API_URL} + NODE ENV {process.env.NODE_ENV}
     </main>
   );
 }
