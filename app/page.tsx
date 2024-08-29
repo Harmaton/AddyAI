4cded69e95a13c4d812bec7a0e58b46044ac19cf
Harmaton <njagiiharmaton@gmail.com>
2024-08-29 11:10:17 +0300

    (Test): Config setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
+  const url = process.env.API_URL
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      ADDY AI
+    Sample env  {url}
     </main>
   );
 }
