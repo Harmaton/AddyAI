f38957c625bd984b1cf474a3c798491b72e08988
Harmaton <njagiiharmaton@gmail.com>
2024-08-31 20:03:11 +0300

    updated the api to fetch emails

--- app/api/inbox/route.ts
+++ app/api/inbox/route.ts
@@ -1,14 +1,14 @@
-import { connect, fetchEmails } from "@/lib/imapclient";
+
 
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
-    const imap = await connect();
-    const emails = await fetchEmails(imap);
-    imap.end();
-    return NextResponse.json(emails, { status: 200 });
+    // const imap = await connect();
+    // const emails = await fetchEmails(imap);
+    // imap.end();
+    return NextResponse.json( { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to fetch emails' }, { status: 500 });
   }
